60d2e3a2618e78f6bf5423c6f62bd34bc083a5f0
Randy Mackay <rmackay9@yahoo.com>
2024-08-03 10:05:06 +0900

    AP_Scripting: add set_pos_offset binding
    also adds set_vel_offset and get_posvel_offset

--- libraries/AP_Scripting/docs/docs.lua
+++ libraries/AP_Scripting/docs/docs.lua
@@ -2344,6 +2344,21 @@ function vehicle:set_circle_rate(rate_dps) end
 ---@return number|nil
 function vehicle:get_circle_radius() end
 
+-- add an offset to vehicle's target position in autonomous modes
+---@param pos_offset_NED Vector3f_ud
+---@return boolean
+function vehicle:set_pos_offset(pos_offset_NED) end
+
+-- add an offset to vehicle's target velocity in autonomous modes
+---@param vel_offset_NED Vector3f_ud
+---@return boolean
+function vehicle:set_vel_offset(vel_offset_NED) end
+
+-- get position and velocity offsets to vehicle's target position in autonomous modes
+---@return Vector3f_ud|nil
+---@return Vector3f_ud|nil
+function vehicle:get_posvel_offset() end
+
 -- desc
 ---@param roll_deg number
 ---@param pitch_deg number
