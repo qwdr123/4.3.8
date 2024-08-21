194e6f654dc5a9d76642665f34dbc35b7a37e3e1
Peter Barker <pbarker@barker.dropbear.id.au>
2024-08-21 16:55:39 +1000

    AP_Proximity: use floats for min/max distances

--- libraries/AP_Proximity/AP_Proximity_RangeFinder.cpp
+++ libraries/AP_Proximity/AP_Proximity_RangeFinder.cpp
@@ -51,8 +51,8 @@ void AP_Proximity_RangeFinder::update(void)
                 const AP_Proximity_Boundary_3D::Face face = frontend.boundary.get_face(angle);
                 // distance in meters
                 const float distance = sensor->distance();
-                _distance_min = sensor->min_distance_cm() * 0.01f;
-                _distance_max = sensor->max_distance_cm() * 0.01f;
+                _distance_min = sensor->min_distance();
+                _distance_max = sensor->max_distance();
                 if ((distance <= _distance_max) && (distance >= _distance_min) && !ignore_reading(angle, distance, false)) {
                     frontend.boundary.set_face_attributes(face, angle, distance, state.instance);
                     // update OA database
