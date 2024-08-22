sayrg994/ardupilot

AP_Camera: Add Camera Tracking functionality

libraries/AP_Camera/AP_Camera_Mount.h
@@ -51,7 +51,7 @@ public:
     // set tracking to none, point or rectangle (see TrackingType enum)
     // if POINT only p1 is used, if RECTANGLE then p1 is top-left, p2 is bottom-right
     // p1,p2 are in range 0 to 1.  0 is left or top, 1 is right or bottom
-    bool set_tracking(TrackingType tracking_type, const Vector2f& p1, const Vector2f& p2) override;
+    bool set_tracking_internal(TrackingType tracking_type, const Vector2f& p1, const Vector2f& p2) override;
 
     // set camera lens as a value from 0 to 5
     bool set_lens(uint8_t lens) override;

libraries/AP_Camera/AP_Camera_Scripting.h
@@ -49,7 +49,7 @@ public:
     // set tracking to none, point or rectangle (see TrackingType enum)
     // if POINT only p1 is used, if RECTANGLE then p1 is top-left, p2 is bottom-right
     // p1,p2 are in range 0 to 1.  0 is left or top, 1 is right or bottom
-    bool set_tracking(TrackingType tracking_type, const Vector2f& p1, const Vector2f& p2) override;
+    bool set_tracking_internal(TrackingType tracking_type, const Vector2f& p1, const Vector2f& p2) override;
 
     // returns true on success and cam_state is filled in
     bool get_state(AP_Camera::camera_state_t& cam_state) override;

qwdr123/4.3.8

libraries/AP_Camera/AP_Camera_Mount.h
@@ -51,7 +51,7 @@ public:
     // set tracking to none, point or rectangle (see TrackingType enum)
     // if POINT only p1 is used, if RECTANGLE then p1 is top-left, p2 is bottom-right
     // p1,p2 are in range 0 to 1.  0 is left or top, 1 is right or bottom
-    bool set_tracking(TrackingType tracking_type, const Vector2f& p1, const Vector2f& p2) override;
+    bool set_tracking_internal(TrackingType tracking_type, const Vector2f& top_left, const Vector2f& bottom_right) override;
 
     // set camera lens as a value from 0 to 5
     bool set_lens(uint8_t lens) override;

libraries/AP_Camera/AP_Camera_Scripting.h
@@ -49,7 +49,7 @@ public:
     // set tracking to none, point or rectangle (see TrackingType enum)
     // if POINT only p1 is used, if RECTANGLE then p1 is top-left, p2 is bottom-right
     // p1,p2 are in range 0 to 1.  0 is left or top, 1 is right or bottom
-    bool set_tracking(TrackingType tracking_type, const Vector2f& p1, const Vector2f& p2) override;
+    bool set_tracking_internal(TrackingType tracking_type, const Vector2f& top_left, const Vector2f& bottom_right) override;
 
     // returns true on success and cam_state is filled in
     bool get_state(AP_Camera::camera_state_t& cam_state) override;